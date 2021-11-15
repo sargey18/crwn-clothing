83cd175c430e8f8c5799d1d1abf8d74b77cf832f
Ryan Sargeson <ryanteachviet@gmail.com>
2021-11-15 18:35:18 +0000

    figured the routing out with react dom 6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import {Routes, Route } from 'react-router-dom';
 import './App.css';
 
 // we import from the homepagecomponent and then render it 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+
 function App() {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+    
+
   );
 }
 
